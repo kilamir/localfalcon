491dc2383364964840857f8a877d3b825f33b16b
Quan M. Nguyen <quan24@gmail.com>
2012-03-31 12:24:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localfalcon::Application.config.secret_token = 'ca771e26faa0a344a13a85e313947680a2780f60e630e6d96c167a2b371f8ce435a0792219862c838b2f2d41baba0c05690f057490c3e8a61575b537c8c26e80'
