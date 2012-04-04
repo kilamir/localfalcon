488f5c2144193631315310dd4c605b6e723dd56c
Quan M. Nguyen <quan24@gmail.com>
2012-04-03 20:06:24 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @title = "#{@user.firstname} #{@user.lastname}"
   end
 
   def new
-    @title = "Sign Up"
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Local Falcon!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
